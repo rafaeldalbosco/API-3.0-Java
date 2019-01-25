5e2c4a32a482fc6e283bb492a8a9de473ee2abde
Rafael F. Dal Bosco <rafaeldalbosco@hotmail.com>
2019-01-24 22:24:46 -0200

    Correção erro codigo 184 - Request could not be empty

--- api30.sdk/src/main/java/cieloecommerce/sdk/ecommerce/request/CreateSaleRequest.java
+++ api30.sdk/src/main/java/cieloecommerce/sdk/ecommerce/request/CreateSaleRequest.java
@@ -3,8 +3,9 @@ package cieloecommerce.sdk.ecommerce.request;
 import java.io.IOException;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.entity.EntityBuilder;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.ContentType;
 
 import com.google.gson.GsonBuilder;
 
@@ -25,7 +26,11 @@ public class CreateSaleRequest extends AbstractSaleRequest<Sale, Sale> {
 		String url = environment.getApiUrl() + "1/sales/";
 		HttpPost request = new HttpPost(url);
 
-		request.setEntity(new StringEntity(new GsonBuilder().create().toJson(param)));
+		EntityBuilder entityBuilder = EntityBuilder.create();
+		entityBuilder.setContentType(ContentType.APPLICATION_JSON);
+		entityBuilder.setContentEncoding("UTF-8");
+		entityBuilder.setText(new GsonBuilder().create().toJson(param));
+		request.setEntity(entityBuilder.build());
 
 		HttpResponse response = sendRequest(request);
 
